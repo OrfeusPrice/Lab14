0504fe55caac6a17ada156c504e593024d801bd9
Приходько Алексей <aprikhodko@sfedu.ru>
2021-10-21 14:48:27 +0300

    Добавлена заглушка для функции LaterInYear в DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,14 @@
-﻿function IsLeapYear(x : integer) : boolean := DateTime.IsLeapYear(x);
-  
+﻿
+
+function IsLeapYear(x : integer) : boolean := DateTime.IsLeapYear(x);
+
+function SecondInMinute() : integer := 60;     //В минуте всегда  60 секунд.
+
+function LaterInYear(DAY1,MONTH1,DAY2,MONTH2 : integer) : integer;
+begin
+  result := 1;
+end;
+
+begin
+
 end.
