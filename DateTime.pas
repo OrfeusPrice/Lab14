107e5630014bb6e8d43d9a5257b939550085feed
Приходько Алексей <aprikhodko@sfedu.ru>
2021-10-21 14:27:54 +0300

    add function SecondInMinute in DateTime.pas

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,10 @@
-﻿function IsLeapYear(x : integer) : boolean := DateTime.IsLeapYear(x);
-  
+﻿
+
+function IsLeapYear(x : integer) : boolean := DateTime.IsLeapYear(x);
+
+function SecondInMinute() : integer := 60;     //В минуте всегда  60 секунд.
+
+
+begin
+
 end.
