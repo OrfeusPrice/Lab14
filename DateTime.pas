16bd9b9f3e96b7d055c6ce8c411f96f848fca0f6
Приходько Алексей <aprikhodko@sfedu.ru>
2021-10-21 15:02:01 +0300

    Добавлена функция DaysInYear, изменена функция IsLeapYear

--- DateTime.pas
+++ DateTime.pas
@@ -1,6 +1,6 @@
 ﻿
 
-function IsLeapYear(x : integer) : boolean := DateTime.IsLeapYear(x);
+function IsLeapYear(year : integer) : boolean := DateTime.IsLeapYear(year);
 
 function SecondInMinute() : integer := 60;     //В минуте всегда  60 секунд.
 
@@ -9,6 +9,14 @@ begin
   result := 1;
 end;
 
+function DaysInYear(year:integer) : integer ;
 begin
+  for var i := 1 to 12 do
+  begin
+    result += datetime.DaysInMonth(year,i);
+  end;
+end;
 
+begin
+writeln (daysinyear(2021));
 end.
