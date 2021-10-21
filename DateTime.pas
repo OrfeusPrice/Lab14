b6bd7befa7edd135cb4294c3453514640781dd18
Приходько Алексей <aprikhodko@sfedu.ru>
2021-10-21 15:06:09 +0300

    Добавлена функция DaysInYearRange

--- DateTime.pas
+++ DateTime.pas
@@ -1,22 +1,32 @@
 ﻿
 
-function IsLeapYear(year : integer) : boolean := DateTime.IsLeapYear(year);
+function IsLeapYear(year: integer): boolean := DateTime.IsLeapYear(year);
 
-function SecondInMinute() : integer := 60;     //В минуте всегда  60 секунд.
 
-function LaterInYear(DAY1,MONTH1,DAY2,MONTH2 : integer) : integer;
+function SecondInMinute(): integer := 60;     //В минуте всегда  60 секунд.
+
+function LaterInYear(DAY1, MONTH1, DAY2, MONTH2: integer): integer;
 begin
   result := 1;
 end;
 
-function DaysInYear(year:integer) : integer ;
+function DaysInYear(year: integer): integer;
 begin
   for var i := 1 to 12 do
   begin
-    result += datetime.DaysInMonth(year,i);
+    result += datetime.DaysInMonth(year, i);
   end;
 end;
 
+function DaysInYearRange(year1, year2: integer): integer;
 begin
-writeln (daysinyear(2021));
+  for var y := year1 to year2 do
+    for var i := 1 to 12 do
+    begin
+      result += datetime.DaysInMonth(y, i);
+    end;
+end;
+
+begin
+
 end.
